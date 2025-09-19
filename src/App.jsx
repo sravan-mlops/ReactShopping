11212df3b04b40fc6fb1dbef3e3000f4aa4f7081
sravan <sravandevops903@gmail.com>
2025-09-19 17:35:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import MainPAge from './shoppingfolder/pages/MainPAge'
+
+const App = () => {
+  return (
+    <div>
+       
+        <MainPAge/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/shoppingfolder/pages/MainPAge.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Collections from '../components/Collections'
+import Footer from '../components/Footer'
+
+const MainPAge = () => {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+        <Collections/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainPAge
